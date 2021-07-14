eac40166795ac84b2d278db30f0dab789d81b4b9
Ori Zaken <zaken1ori@gmail.com>
2021-07-14 21:05:41 +0300

    21:05 ori

--- src/views/yacht-details.vue
+++ src/views/yacht-details.vue
@@ -1,131 +1,131 @@
-// <template>
-//   <section>
-//     <div class="container">
-//       <header class="header">
-//         <h1 class="yacht-name">Sailboat Jeanneau Sun Odyssey 509 (2014)</h1>
-//       </header>
+<template>
+  <section>
+    <div class="container">
+      <header class="header">
+        <h1 class="yacht-name">Sailboat Jeanneau Sun Odyssey 509 (2014)</h1>
+      </header>
 
-//       <div class="images-Container">
-//         <img src="./assets/logo.png" />
-//         <img src="./assets/logo.png" />
-//         <img src="./assets/logo.png" />
-//       </div>
+      <div class="images-Container">
+        <!-- <img src="./assets/logo.jpg" />
+        <img src="./assets/logo.jpg" />
+        <img src="./assets/logo.jpg" /> -->
+      </div>
 
-//       <div class="productView__article">
-//         <section class="productView__block">
-//           <section class="productGeneral">
-//             <div class="productGeneral__rowContainer">
-//               <h1 class="productGeneral__boat">
-//                 Yacht charter in Tivat
-//                 <br />
-//                 <span class="productGeneral__title js-product-title">
-//                   SAVER — Open 580 (2017)
-//                 </span>
-//               </h1>
+      <div class="productView__article">
+        <section class="productView__block">
+          <section class="productGeneral">
+            <div class="productGeneral__rowContainer">
+              <h1 class="productGeneral__boat">
+                Yacht charter in Tivat
+                <br />
+                <span class="productGeneral__title js-product-title">
+                  SAVER — Open 580 (2017)
+                </span>
+              </h1>
 
-//               <div
-//                 class="productGeneral__imgContainer"
-//                 data-module="profile"
-//                 data-action="display"
-//                 data-account-id="89396"
-//                 data-product-id="19123"
-//               >
-//                 <img
-//                   data-src="//static1.clickandboat.com/v1/a/g6I2tztu0epIlJlBmrx6wTMHF4EKrogC.big.jpg"
-//                   alt="Dean"
-//                   class="productGeneral__img lazy"
-//                 />
-//               </div>
-//             </div>
+              <div
+                class="productGeneral__imgContainer"
+                data-module="profile"
+                data-action="display"
+                data-account-id="89396"
+                data-product-id="19123"
+              >
+                <img
+                  data-src="//static1.clickandboat.com/v1/a/g6I2tztu0epIlJlBmrx6wTMHF4EKrogC.big.jpg"
+                  alt="Dean"
+                  class="productGeneral__img lazy"
+                />
+              </div>
+            </div>
 
-//             <div class="productGeneral__rowContainer">
-//               <div class="productGeneral__informations">
-//                 <div class="productGeneral__reviews">
-//                   <a
-//                     href=""
-//                     class="productGeneral__stars js-anchor"
-//                     data-anchor-selector="#global-review"
-//                     data-action="scroll"
-//                     data-speed="slow"
-//                   >
-//                     <div class="newRatingStarsContainer--medium">
-//                       <img
-//                         loading="lazy"
-//                         src="//static1.clickandboat.com/v1/o/img/svg/stars~ebed3586da.svg.gz#star-5"
-//                         alt="rating-stars"
-//                       />
-//                     </div>
-//                   </a>
+            <div class="productGeneral__rowContainer">
+              <div class="productGeneral__informations">
+                <div class="productGeneral__reviews">
+                  <a
+                    href=""
+                    class="productGeneral__stars js-anchor"
+                    data-anchor-selector="#global-review"
+                    data-action="scroll"
+                    data-speed="slow"
+                  >
+                    <div class="newRatingStarsContainer--medium">
+                      <img
+                        loading="lazy"
+                        src="//static1.clickandboat.com/v1/o/img/svg/stars~ebed3586da.svg.gz#star-5"
+                        alt="rating-stars"
+                      />
+                    </div>
+                  </a>
 
-//                   <a
-//                     href=""
-//                     class="productGeneral__text js-anchor"
-//                     data-anchor-selector="#commentaires"
-//                     data-action="scroll"
-//                     data-speed="slow"
-//                   >
-//                     (41)
-//                   </a>
-//                 </div>
+                  <a
+                    href=""
+                    class="productGeneral__text js-anchor"
+                    data-anchor-selector="#commentaires"
+                    data-action="scroll"
+                    data-speed="slow"
+                  >
+                    (41)
+                  </a>
+                </div>
 
-//                 <div class="productGeneral__infos">
-//                   <div
-//                     class="productGeneral__owner"
-//                     data-module="profile"
-//                     data-action="display"
-//                     data-account-id="89396"
-//                     data-product-id="19123"
-//                   >
-//                     Offered by
-//                     <a
-//                       href="#"
-//                       class="productGeneral__text--mgleft js-owner-name"
-//                       >Dean</a
-//                     >
-//                   </div>
+                <div class="productGeneral__infos">
+                  <div
+                    class="productGeneral__owner"
+                    data-module="profile"
+                    data-action="display"
+                    data-account-id="89396"
+                    data-product-id="19123"
+                  >
+                    Offered by
+                    <a
+                      href="#"
+                      class="productGeneral__text--mgleft js-owner-name"
+                      >Dean</a
+                    >
+                  </div>
 
-//                   <div class="productGeneral__text--grey">Tivat, Tivat</div>
-//                 </div>
-//               </div>
+                  <div class="productGeneral__text--grey">Tivat, Tivat</div>
+                </div>
+              </div>
 
-//               <div
-//                 class="productGeneral__imgContainer--onMobile"
-//                 data-module="profile"
-//                 data-action="display"
-//                 data-account-id="89396"
-//                 data-product-id="19123"
-//               >
-//                 <img
-//                   data-src="//static1.clickandboat.com/v1/a/g6I2tztu0epIlJlBmrx6wTMHF4EKrogC.big.jpg"
-//                   alt="Dean"
-//                   class="productGeneral__img lazy"
-//                 />
-//               </div>
-//             </div>
+              <div
+                class="productGeneral__imgContainer--onMobile"
+                data-module="profile"
+                data-action="display"
+                data-account-id="89396"
+                data-product-id="19123"
+              >
+                <img
+                  data-src="//static1.clickandboat.com/v1/a/g6I2tztu0epIlJlBmrx6wTMHF4EKrogC.big.jpg"
+                  alt="Dean"
+                  class="productGeneral__img lazy"
+                />
+              </div>
+            </div>
 
-//             <div class="productGeneral__equipments">
-//               <div class="productGeneral__text--med bht-boat-capacity">
-//                 6 people
-//               </div>
+            <div class="productGeneral__equipments">
+              <div class="productGeneral__text--med bht-boat-capacity">
+                6 people
+              </div>
 
-//               <div class="productGeneral__text--med">
-//                 &nbsp;· 100 horsepower
-//               </div>
-//               <div class="productGeneral__text--med">&nbsp;· 5.8 metres</div>
-//             </div>
-//           </section>
-//         </section>
+              <div class="productGeneral__text--med">
+                &nbsp;· 100 horsepower
+              </div>
+              <div class="productGeneral__text--med">&nbsp;· 5.8 metres</div>
+            </div>
+          </section>
+        </section>
 
-//         <section class="productView__block"></section>
-//         <section class="productView__block"></section>
-//         <section class="productView__block"></section>
-//         <section class="productView__block"></section>
-//         <section class="productView__block"></section>
-//       </div>
-//     </div>
-//   </section>
-// </template>
+        <section class="productView__block"></section>
+        <section class="productView__block"></section>
+        <section class="productView__block"></section>
+        <section class="productView__block"></section>
+        <section class="productView__block"></section>
+      </div>
+    </div>
+  </section>
+</template>
 
-// <script>
-// export default {};
-// </script>
+<script>
+export default {};
+</script>
