ca66aa480f6dadfb775be67922cdfdacb7bd4b96
Ori Zaken <zaken1ori@gmail.com>
2021-07-16 09:24:44 +0300

    ori 9:24

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import './styles/styles.scss'
 import locale from 'element-ui/lib/locale/lang/en'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI, { locale })
 
+Vue.use(ElementUI, { locale })
 
 
 const GOOGLE_API_KEY = '' // need to secure 
